0ceaf03e79adc49acd13bc27235159a60d46f047
Chris Hiestand <chrishiestand@gmail.com>
2010-07-02 20:17:35 -0700

    *whitespace changes only

--- Utilities.php
+++ Utilities.php
@@ -352,7 +352,7 @@
 	}
   
   function gmt_datetime($timestamp) {
-    return gmdate( 'D, d M Y H:i:s \G\M\T', $timestamp);
+    return gmdate('D, d M Y H:i:s \G\M\T', $timestamp);
   }
 
 	function http_expire_now() {
@@ -362,7 +362,7 @@
 		 */
 		assert(!headers_sent());
 		header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");									// Date in the past
-		header("Last-Modified: " . gmt_datetime(time()));													// always modified
+		header("Last-Modified: " . gmt_datetime(time()));									// always modified
 		header("Cache-Control: no-cache, must-revalidate");								// HTTP/1.1
 		header("Pragma: no-cache");																				// HTTP/1.0
 	}
